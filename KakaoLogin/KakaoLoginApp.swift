d2bd70dd4ffd95caa5b24351a09eb9061ca1fe86
zoop1259 <kdm43211991@gmail.com>
2023-03-14 14:48:08 +0900

    Initial Commit

---
+++ KakaoLogin/KakaoLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakaoLoginApp.swift
+//  KakaoLogin
+//
+//  Created by 강대민 on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct KakaoLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
